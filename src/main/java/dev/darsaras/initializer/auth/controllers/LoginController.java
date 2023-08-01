4bd45d042d433277db58a4faf01c042c98d1e649
DanteDeLordran <lordran@lordran.dev>
2023-08-01 14:36:58 -0600

    Updated repository and build LoginService

---
+++ src/main/java/dev/darsaras/initializer/auth/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package dev.darsaras.initializer.auth.controllers;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.darsaras.initializer.auth.models.User;
+import dev.darsaras.initializer.auth.services.LoginService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class LoginController {
+    
+    private final LoginService loginService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerCustomer( @RequestBody User user ){
+        return loginService.registerUser(user);
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<Optional<User>> getUserDetailsAfterLogin(Authentication authentication){
+        return loginService.getUserDetailsAfterLogin(authentication);
+    }
+
+}
---
+++ src/main/java/dev/darsaras/initializer/auth/services/LoginService.java
@@ -0,0 +1,50 @@
+package dev.darsaras.initializer.auth.services;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import dev.darsaras.initializer.auth.models.User;
+import dev.darsaras.initializer.auth.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ResponseEntity<String> registerUser( User user) {
+        try {
+            String hashedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(hashedPassword);
+            userRepository.save(user);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body("Given user details are successfully registered");
+
+        } catch (Exception ex) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+    
+    }
+
+    //This method helps us to store the Customer details on the session storage on the frontend
+    public ResponseEntity<Optional<User>> getUserDetailsAfterLogin(Authentication authentication){
+        Optional<User> customer = userRepository.findByEmail(authentication.getName());
+        if (customer.isPresent()) {
+            return ResponseEntity.ok(customer);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+}
