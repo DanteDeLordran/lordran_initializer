3238454a71f8ec020edad87bf86d096391b07563
DanteDeLordran <lordran@lordran.dev>
2023-08-01 10:15:33 -0600

    Initialized project

---
+++ src/test/java/dev/darsaras/initializer/InitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.darsaras.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
