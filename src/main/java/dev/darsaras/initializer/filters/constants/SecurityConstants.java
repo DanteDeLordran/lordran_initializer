ef85bf3d617ce85bdc145fd345c9fc7c77df28f1
DanteDeLordran <lordran@lordran.dev>
2023-08-01 20:14:38 -0600

    JWT generator and validator filters

---
+++ src/main/java/dev/darsaras/initializer/filters/constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package dev.darsaras.initializer.filters.constants;
+
+public interface SecurityConstants {
+    public static final String JWT_KEY = "ddadlskfalbvsubdvb";
+    public static final String JWT_HEADER = "Authorization";
+}
